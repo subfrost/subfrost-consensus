549dbc7ea64a9b83a336322cb73cf24562013bca
kungfuflex <judoflexchop@gmail.com>
2024-10-06 21:47:22 +0000

    transfer_from

--- src/parcel.rs
+++ src/parcel.rs
@@ -1,6 +1,6 @@
 use crate::id::AlkaneId;
 use crate::utils::consume_sized_int;
-use anyhow::Result;
+use anyhow::{Result, anyhow};
 use metashrew::index_pointer::{AtomicPointer, IndexPointer, KeyValuePointer};
 
 #[derive(Default, Clone)]
@@ -26,6 +26,14 @@ impl AlkaneTransferParcel {
         from: &AlkaneId,
         to: &AlkaneId,
     ) -> Result<()> {
+        for transfer in &self.0 {
+          let balance = pointer.keyword("/alkanes/").select(&transfer.id.into()).keyword("/balances/").select(&from.into()).get_value::<u128>();
+          if balance < transfer.value {
+            return Err(anyhow!("balance underflow"))
+          }
+          pointer.keyword("/alkanes/").select(&transfer.id.into()).keyword("/balances/").select(&from.into()).set_value::<u128>(balance - transfer.value);
+          pointer.keyword("/alkanes/").select(&transfer.id.into()).keyword("/balances/").select(&to.into()).set_value::<u128>(balance + transfer.value);
+        }
         Ok(())
     }
 }
