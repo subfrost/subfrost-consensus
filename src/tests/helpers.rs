224b10769f704557147823efc200099b1f5c1e86
v16s <16210686+v16s@users.noreply.github.com>
2024-10-19 23:19:43 +0530

    now builds sample-alkane as a dep and uses it in test

--- src/tests/helpers.rs
+++ src/tests/helpers.rs
@@ -1,27 +1,11 @@
 use anyhow::Result;
-use std::{fs, process::Command};
+use std::fs;
 
 pub fn read_sample_contract() -> Result<Vec<u8>> {
-    let lib_path = "./sample_contract";
-    let target_dir = "./sample_contract/target/wasm32-unknown-unknown/debug";
-
-    // Run `cargo build` for the wasm_lib targeting WASM.
-    let status = Command::new("cargo")
-        .arg("build")
-        .arg("--target")
-        .arg("wasm32-unknown-unknown")
-        .current_dir(lib_path)
-        .status()?;
-
-    println!("built");
-
-    // Check if the build was successful.
-    if !status.success() {
-        panic!("Failed to compile the wasm_lib to WebAssembly");
-    }
+    let target_dir = "./target/wasm32-unknown-unknown/debug/deps";
 
     // Step 2: Load the compiled `.wasm` file into memory.
-    let wasm_file = format!("{}/sample_contract.wasm", target_dir);
+    let wasm_file = format!("{}/sample-alkane.wasm", target_dir);
     let wasm_binary = fs::read(wasm_file)?;
     Ok(wasm_binary)
 }
