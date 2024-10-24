c18701d4b9872377d6b3e498badc3d349f743de6
v16s <16210686+v16s@users.noreply.github.com>
2024-10-24 18:13:56 +0530

    changes to imports

--- crates/alkanes-runtime/src/imports.rs
+++ crates/alkanes-runtime/src/imports.rs
@@ -1,15 +1,15 @@
 #[link(wasm_import_module = "env")]
 extern "C" {
     pub fn abort() -> i32;
-    pub fn __load_storage(k: i32, v: i32);
+    pub fn __load_storage(k: i32, v: i32) -> i32;
     pub fn __request_storage(k: i32) -> i32;
     pub fn __log(v: i32);
     pub fn __balance(who: i32, what: i32, output: i32);
     pub fn __request_context() -> i32;
     pub fn __load_context(output: i32) -> i32;
     pub fn __sequence(output: i32) -> i32;
-    pub fn __fuel(output: i32) -> i32;
-    pub fn __returndatacopy(output: i32) -> i32;
+    pub fn __fuel(output: i32);
+    pub fn __returndatacopy(output: i32);
     pub fn __request_transaction() -> i32;
     pub fn __load_transaction(output: i32);
     pub fn __request_block() -> i32;
