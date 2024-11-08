1d1c35544a23b064bd103c09c0e4a05c95497399
v16s <16210686+v16s@users.noreply.github.com>
2024-11-09 00:04:53 +0530

    working changes

--- crates/alkanes-runtime/src/compat.rs
+++ crates/alkanes-runtime/src/compat.rs
@@ -1,6 +1,6 @@
 use crate::{println, stdio::stdout};
 use std::fmt::Write;
 use std::panic;
-pub fn panic_hook(info: &panic::PanicHookInfo) {
+pub fn panic_hook(info: &panic::PanicInfo) {
     println!("panicked with error: {}", info.to_string());
 }
