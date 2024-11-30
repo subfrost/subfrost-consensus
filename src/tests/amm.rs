61d56b645583f16aed78a36429131bad6640484c
ksyao <ksyao2002@gmail.com>
2024-11-29 17:49:10 -0800

    init

--- src/tests/amm.rs
+++ src/tests/amm.rs
@@ -7,7 +7,7 @@ use anyhow::Result;
 use bitcoin::address::NetworkChecked;
 use bitcoin::blockdata::transaction::OutPoint;
 use bitcoin::hashes::Hash;
-use bitcoin::{Address, Amount, ScriptBuf, Sequence, TxIn, TxOut, Witness};
+use bitcoin::{Address, Amount, Block, ScriptBuf, Sequence, TxIn, TxOut, Witness};
 use metashrew_support::index_pointer::KeyValuePointer;
 use protobuf::{Message, MessageField};
 use protorune::{
@@ -26,10 +26,19 @@ use crate::tests::std::{alkanes_std_amm_factory_build, alkanes_std_owned_token_b
 use metashrew::{clear, get_cache, index_pointer::IndexPointer, println, stdio::stdout};
 use std::fmt::Write;
 use wasm_bindgen_test::wasm_bindgen_test;
-#[wasm_bindgen_test]
-fn test_amm_pool_normal() -> Result<()> {
-    clear();
-    let block_height = 840_000;
+
+struct AmmTestDeploymentIds {
+    amm_pool_factory: AlkaneId,
+    auth_token_factory: AlkaneId,
+    amm_factory_deployment: AlkaneId,
+    owned_token_1_deployment: AlkaneId,
+    auth_token_1_deployment: AlkaneId,
+    owned_token_2_deployment: AlkaneId,
+    auth_token_2_deployment: AlkaneId,
+    amm_pool_deployment: AlkaneId,
+}
+
+fn init_block_with_amm_pool() -> Result<(Block, AmmTestDeploymentIds)> {
     let cellpacks: Vec<Cellpack> = [
         //amm pool factory init
         Cellpack {
@@ -63,20 +72,7 @@ fn test_amm_pool_normal() -> Result<()> {
         },
     ]
     .into();
-    let amm_pool_factory = AlkaneId {
-        block: 4,
-        tx: AMM_FACTORY_ID,
-    };
-    let auth_token_factory = AlkaneId {
-        block: 4,
-        tx: AUTH_TOKEN_FACTORY_ID,
-    };
-    let amm_factory_deployment = AlkaneId { block: 2, tx: 0 };
-    let owned_token_1_deployment = AlkaneId { block: 2, tx: 1 };
-    let auth_token_1_deployment = AlkaneId { block: 2, tx: 2 };
-    let owned_token_2_deployment = AlkaneId { block: 2, tx: 3 };
-    let auth_token_2_deployment = AlkaneId { block: 2, tx: 4 };
-    let mut test_block = alkane_helpers::init_with_multiple_cellpacks_with_tx(
+    let test_block = alkane_helpers::init_with_multiple_cellpacks_with_tx(
         [
             alkanes_std_amm_pool_build::get_bytes(),
             alkanes_std_auth_token_build::get_bytes(),
@@ -87,15 +83,79 @@ fn test_amm_pool_normal() -> Result<()> {
         .into(),
         cellpacks,
     );
+    let deployed_ids = AmmTestDeploymentIds {
+        amm_pool_factory: AlkaneId {
+            block: 4,
+            tx: AMM_FACTORY_ID,
+        },
+        auth_token_factory: AlkaneId {
+            block: 4,
+            tx: AUTH_TOKEN_FACTORY_ID,
+        },
+        amm_factory_deployment: AlkaneId { block: 2, tx: 0 },
+        owned_token_1_deployment: AlkaneId { block: 2, tx: 1 },
+        auth_token_1_deployment: AlkaneId { block: 2, tx: 2 },
+        owned_token_2_deployment: AlkaneId { block: 2, tx: 3 },
+        auth_token_2_deployment: AlkaneId { block: 2, tx: 4 },
+        amm_pool_deployment: AlkaneId { block: 2, tx: 5 },
+    };
+    return Ok((test_block, deployed_ids));
+}
+
+fn assert_contracts_correct_ids(deployment_ids: &AmmTestDeploymentIds) -> Result<()> {
+    let _ = assert_binary_deployed_to_id(
+        deployment_ids.amm_pool_factory.clone(),
+        alkanes_std_amm_pool_build::get_bytes(),
+    );
+    let _ = assert_binary_deployed_to_id(
+        deployment_ids.auth_token_factory.clone(),
+        alkanes_std_auth_token_build::get_bytes(),
+    );
+
+    let _ = assert_binary_deployed_to_id(
+        deployment_ids.amm_factory_deployment.clone(),
+        alkanes_std_amm_factory_build::get_bytes(),
+    );
+    let _ = assert_binary_deployed_to_id(
+        deployment_ids.owned_token_1_deployment.clone(),
+        alkanes_std_owned_token_build::get_bytes(),
+    );
+    let _ = assert_binary_deployed_to_id(
+        deployment_ids.owned_token_2_deployment.clone(),
+        alkanes_std_owned_token_build::get_bytes(),
+    );
+    let _ = assert_binary_deployed_to_id(
+        deployment_ids.auth_token_1_deployment.clone(),
+        alkanes_std_auth_token_build::get_bytes(),
+    );
+    let _ = assert_binary_deployed_to_id(
+        deployment_ids.auth_token_2_deployment.clone(),
+        alkanes_std_auth_token_build::get_bytes(),
+    );
+    let _ = assert_binary_deployed_to_id(
+        deployment_ids.amm_pool_deployment.clone(),
+        alkanes_std_amm_pool_build::get_bytes(),
+    );
+    Ok(())
+}
+
+fn insert_add_liquidity_tx(
+    amount1: u128,
+    amount2: u128,
+    test_block: &mut Block,
+    deployment_ids: &AmmTestDeploymentIds,
+) {
+    let input_txid = test_block.txdata[test_block.txdata.len() - 1].compute_txid();
+    let previous_output = OutPoint {
+        txid: input_txid,
+        vout: 0,
+    };
     let address: Address<NetworkChecked> =
         protorune::test_helpers::get_address(&protorune::test_helpers::ADDRESS1);
     let script_pubkey = address.script_pubkey();
     let split = alkane_helpers::create_protostone_tx_with_inputs(
         vec![TxIn {
-            previous_output: OutPoint {
-                txid: test_block.txdata[test_block.txdata.len() - 1].compute_txid(),
-                vout: 0,
-            },
+            previous_output: previous_output.clone(),
             script_sig: ScriptBuf::new(),
             sequence: Sequence::MAX,
             witness: Witness::new(),
@@ -119,13 +179,13 @@ fn test_amm_pool_normal() -> Result<()> {
             refund: None,
             edicts: vec![
                 ProtostoneEdict {
-                    id: owned_token_1_deployment.into(),
-                    amount: 1000000,
+                    id: deployment_ids.owned_token_1_deployment.into(),
+                    amount: amount1,
                     output: 0,
                 },
                 ProtostoneEdict {
-                    id: owned_token_2_deployment.into(),
-                    amount: 1000000,
+                    id: deployment_ids.owned_token_2_deployment.into(),
+                    amount: amount2,
                     output: 0,
                 },
             ],
@@ -136,63 +196,37 @@ fn test_amm_pool_normal() -> Result<()> {
         alkane_helpers::create_multiple_cellpack_with_witness_and_in(
             Witness::new(),
             vec![Cellpack {
-                target: amm_factory_deployment,
+                target: deployment_ids.amm_factory_deployment,
                 inputs: vec![1],
             }],
-            OutPoint {
-                txid: test_block.txdata[test_block.txdata.len() - 1].compute_txid(),
-                vout: 0,
-            },
+            previous_output.clone(),
             false,
         ),
     );
-    let amm_pool_deployment = AlkaneId { block: 2, tx: 5 };
+}
+
+#[wasm_bindgen_test]
+fn test_amm_pool_normal() -> Result<()> {
+    clear();
+    let block_height = 840_000;
+    let (mut test_block, deployment_ids) = init_block_with_amm_pool()?;
+    insert_add_liquidity_tx(1000000, 1000000, &mut test_block, &deployment_ids);
+    index_block(&test_block, block_height)?;
+    assert_contracts_correct_ids(&deployment_ids)?;
     let len = test_block.txdata.len();
     let outpoint = OutPoint {
         txid: test_block.txdata[len - 1].compute_txid(),
         vout: 0,
     };
-
-    index_block(&test_block, block_height)?;
     let ptr = RuneTable::for_protocol(AlkaneMessageContext::protocol_tag())
         .OUTPOINT_TO_RUNES
         .select(&consensus_encode(&outpoint)?);
     let sheet = load_sheet(&ptr);
-    let _ = assert_binary_deployed_to_id(
-        amm_pool_factory.clone(),
-        alkanes_std_amm_pool_build::get_bytes(),
-    );
-    let _ = assert_binary_deployed_to_id(
-        auth_token_factory.clone(),
-        alkanes_std_auth_token_build::get_bytes(),
-    );
-
-    let _ = assert_binary_deployed_to_id(
-        amm_factory_deployment.clone(),
-        alkanes_std_amm_factory_build::get_bytes(),
-    );
-    let _ = assert_binary_deployed_to_id(
-        owned_token_1_deployment.clone(),
-        alkanes_std_owned_token_build::get_bytes(),
-    );
-    let _ = assert_binary_deployed_to_id(
-        owned_token_2_deployment.clone(),
-        alkanes_std_owned_token_build::get_bytes(),
-    );
-    let _ = assert_binary_deployed_to_id(
-        auth_token_1_deployment.clone(),
-        alkanes_std_auth_token_build::get_bytes(),
-    );
-    let _ = assert_binary_deployed_to_id(
-        auth_token_2_deployment.clone(),
-        alkanes_std_auth_token_build::get_bytes(),
-    );
-    let _ = assert_binary_deployed_to_id(
-        amm_pool_deployment.clone(),
-        alkanes_std_amm_pool_build::get_bytes(),
-    );
     println!("balances at end {:?}", sheet);
-    assert_eq!(sheet.get(&amm_pool_deployment.into()), 999000);
+    assert_eq!(
+        sheet.get(&deployment_ids.amm_pool_deployment.into()),
+        999000
+    );
     Ok(())
 }
 
--- src/tests/helpers.rs
+++ src/tests/helpers.rs
@@ -250,5 +250,6 @@ pub fn assert_binary_deployed_to_id(token_id: AlkaneId, binary: Vec<u8>) -> Resu
         .clone();
     let binary_2 = compress(binary.into())?;
     assert_eq!(binary_1.len(), binary_2.len());
+    assert_eq!(binary_1, binary_2);
     return Ok(());
 }
